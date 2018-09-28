793e4962ca1b3ec159647757f3da3c5e9e274375
raywu6 <rwu6@stuy.edu>
2018-09-28 15:20:01 -0400

    add header file

---
+++ str.h
@@ -0,0 +1,11 @@
+int mystrlen ( char * );
+
+char * mystrcpy ( char * , char * );
+
+char * mystrncat ( char * , char * , int );
+
+int mystrcmp ( char * , char * );
+
+char * mystrchr ( char * , char );
+
+char * mystrstr ( char * , char * );
