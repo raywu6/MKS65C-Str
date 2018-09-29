2d89c3ea68d193585352a07752b028a29cfce3f6
Bill <bni@stuy.edu>
2018-09-29 16:29:22 -0400

    added strcpy

--- str.c
+++ str.c
@@ -11,9 +11,22 @@ int mystrlen(char * s) {
   return retInt;
 }
 
+char * mystrcpy ( char * s1, char * s2){
+  int i = 0;
+  int len = mystrlen(s2);
+  for(; i < len; i++){
+    s1[i] = s2[i];
+  }
+  return s1;
+}
+
 int main() {
-  char * s0   = "hello";
+  char s0[10] = "hello";
   char s1[10] = "goodbye";
+  char s2[10] = "test";
+
+  printf("s0 = %s\n", s0);
+  printf("s1 = %s\n\n", s1);
   
   printf("Testing strlen():\n");
   printf("[standard]: %lu\n", strlen(s0));
@@ -21,6 +34,13 @@ int main() {
 
   printf("[standard]: %lu\n", strlen(s1));
   printf("[mine]: %d\n\n", mystrlen(s1));
+
+  printf("s2 = %s\n\n", s2);
+
+  printf("Testing strcpy():\n");
+  printf("[standard]: %s\n", strcpy(s2, s1));
+  printf("[reset]: %s\n", strcpy(s2, "test"));
+  printf("[mine]: %s\n", strcpy(s2, s1));
 
   return 0;
 }
